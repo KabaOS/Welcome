7dc3909a34c98fca28a4d287fedbb73324263db5
Arthur Melton <git-zl9limki@dontrackme.com>
2024-07-22 06:40:02 -0400

    Add a little warning that they might have to wait

--- src/pages/finish.zig
+++ src/pages/finish.zig
@@ -3,7 +3,15 @@ const window = @import("../main.zig").global_window;
 
 pub fn page(_: bool) void {
     const box = c.gtk_box_new(c.GTK_ORIENTATION_VERTICAL, 0);
-    c.gtk_box_append(@ptrCast(box), c.gtk_label_new("Thank you for using Kaba!"));
+    const thankyou_text = c.gtk_label_new("Thank you for using Kaba!");
+    c.gtk_widget_set_margin_top(thankyou_text, 12);
+    c.gtk_box_append(@ptrCast(box), thankyou_text);
+
+    const info_text = c.gtk_label_new("Just know that it usually takes a minute or two to be able to connect to b32 addresses, and another minute or two to be able to connect to the domains you know and love!");
+    c.gtk_label_set_wrap(@ptrCast(info_text), @intFromBool(true));
+    c.gtk_widget_set_margin_top(info_text, 12);
+    c.gtk_box_append(@ptrCast(box), info_text);
+
     c.gtk_window_set_child(@ptrCast(window.window.?), box);
 
     c.gtk_window_set_title(@ptrCast(window.window.?), "Thank You");
