433d749d7b79a970f8214f8484a910df5d33fdc2
Arthur Melton <git-zl9limki@dontrackme.com>
2024-07-23 06:58:45 -0400

    Add margin to left and right of long end text

--- src/pages/finish.zig
+++ src/pages/finish.zig
@@ -10,6 +10,8 @@ pub fn page(_: bool) void {
     const info_text = c.gtk_label_new("Just know that it usually takes a minute or two to be able to connect to b32 addresses, and another minute or two to be able to connect to the domains you know and love!");
     c.gtk_label_set_wrap(@ptrCast(info_text), @intFromBool(true));
     c.gtk_widget_set_margin_top(info_text, 12);
+    c.gtk_widget_set_margin_start(info_text, 12);
+    c.gtk_widget_set_margin_end(info_text, 12);
     c.gtk_box_append(@ptrCast(box), info_text);
 
     c.gtk_window_set_child(@ptrCast(window.window.?), box);
